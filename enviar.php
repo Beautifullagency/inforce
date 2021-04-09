1c94aff3ccee8ec8c79aee1c9123803333a97f87
Beautifullagency <Javiercossio@beautifullagency.com>
2021-04-09 18:20:34 -0300

    fixes

--- enviar.php
+++ enviar.php
@@ -5,6 +5,47 @@ $mail = $_POST['email'];
 $phone = $_POST['phone'];
 $msj = $_POST['mensaje'];
 
+
+$uploadStatus = 1;
+            
+// Upload attachment file
+if(!empty($_FILES["attachment"]["name"])){
+    
+    // File path config
+    $targetDir = "uploads/";
+    $fileName = basename($_FILES["attachment"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+    
+    // Allow certain file formats
+    $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to the server
+        if(move_uploaded_file($_FILES["attachment"]["tmp_name"], $targetFilePath)){
+            $uploadedFile = $targetFilePath;
+        }else{
+            $uploadStatus = 0;
+            $statusMsg = "Sorry, there was an error uploading your file.";
+        }
+    }else{
+        $uploadStatus = 0;
+        $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
